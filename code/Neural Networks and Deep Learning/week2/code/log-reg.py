82c49d1ea3dceba7cc3dc74a9994438d2193fb63
vickykatoch <vickykatoch@yahoo.com>
2020-10-30 07:32:45 -0400

    changes

---
+++ code/Neural Networks and Deep Learning/week2/code/log-reg.py
@@ -0,0 +1,188 @@
+import numpy as np
+from lr_utils import load_dataset
+
+def init_weights_and_bias_with_zeros(dim):
+    w = np.zeros((dim,1))
+    b = 0
+    return w, b
+
+def sigmoid(z):
+    return 1 /(1 + np.exp(-z))
+
+def calc_loss(Y_hat, y):
+    return y * np.log(Y_hat) + (1 - y) * np.log(1 - Y_hat)
+
+def compute_cost(Y_hat, y, m):
+    return -(1/m) * np.sum(calc_loss(Y_hat, y))
+
+def compute_activation(w, X, b):
+    return sigmoid(np.dot(w.T, X)+b)
+
+def propagate(w, b, X, Y):
+    m = X.shape[1]
+    Y_hat = compute_activation(w,X,b)
+    cost = compute_cost(Y_hat, Y, m)
+    
+    # BACKWARD PROPAGATION (TO FIND GRADIENT)
+    dw = (1/m) * np.dot(X, (Y_hat - Y).T)
+    db = (1/m) * np.sum(Y_hat - Y)
+    assert(dw.shape == w.shape)
+    assert(db.dtype == float)
+    cost = np.squeeze(cost)
+    assert(cost.shape == ())
+    grads = {
+        "dw" : dw,
+        "db" : db
+    }
+    return  grads, cost
+
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
+    #This function optimizes w and b by running a gradient descent algorithm
+    costs = []
+    for i in range(num_iterations):
+        # Cost and gradient calculation
+        grads, cost = propagate(w, b, X, Y)
+        
+        # Retrieve derivatives from grads
+        dw = grads["dw"]
+        db = grads["db"]
+        
+        w = w - learning_rate * dw  # need to broadcast
+        b = b - learning_rate * db
+        
+        # Record the costs
+        if i % 100 == 0:
+            costs.append(cost)
+        
+        # Print the cost every 100 training examples
+        if print_cost and i % 100 == 0:
+            print ("Cost after iteration %i: %f" % (i, cost))
+    
+    params = {"w": w,
+              "b": b}
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return params, grads, costs
+
+def predict(w, b, X):
+    '''
+    Predict whether the label is 0 or 1 using learned logistic regression parameters (w, b)
+    
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of size (num_px * num_px * 3, number of examples)
+    
+    Returns:
+    Y_prediction -- a numpy array (vector) containing all predictions (0/1) for the examples in X
+    '''    
+    m = X.shape[1]
+    Y_prediction = np.zeros((1, m))
+    w = w.reshape(X.shape[0], 1)
+    
+    # Compute vector "A" predicting the probabilities of a cat being present in the picture
+    A = sigmoid(np.dot(w.T, X) + b)
+    
+    for i in range(A.shape[1]):
+        # Convert probabilities a[0,i] to actual predictions p[0,i]
+        Y_prediction[0, i] = 1 if A[0, i] > 0.5 else 0
+    
+    assert(Y_prediction.shape == (1, m))
+    
+    return Y_prediction
+
+def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0.5, print_cost=False):
+    """
+    Builds the logistic regression model by calling the function you've implemented previously
+    
+    Arguments:
+    X_train -- training set represented by a numpy array of shape (num_px * num_px * 3, m_train)
+    Y_train -- training labels represented by a numpy array (vector) of shape (1, m_train)
+    X_test -- test set represented by a numpy array of shape (num_px * num_px * 3, m_test)
+    Y_test -- test labels represented by a numpy array (vector) of shape (1, m_test)
+    num_iterations -- hyperparameter representing the number of iterations to optimize the parameters
+    learning_rate -- hyperparameter representing the learning rate used in the update rule of optimize()
+    print_cost -- Set to true to print the cost every 100 iterations
+    
+    Returns:
+    d -- dictionary containing information about the model.
+    """
+    
+    ### START CODE HERE ###
+    # initialize parameters with zeros (≈ 1 line of code)
+    w, b = init_weights_and_bias_with_zeros(X_train.shape[0])
+
+    # Gradient descent (≈ 1 line of code)
+    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
+    
+    # Retrieve parameters w and b from dictionary "parameters"
+    w = parameters["w"]
+    b = parameters["b"]
+    
+    # Predict test/train set examples (≈ 2 lines of code)
+    Y_prediction_test = predict(w, b, X_test)
+    Y_prediction_train = predict(w, b, X_train)
+
+    ### END CODE HERE ###
+
+    # Print train/test Errors
+    print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
+    print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+
+    
+    d = {"costs": costs,
+         "Y_prediction_test": Y_prediction_test, 
+         "Y_prediction_train" : Y_prediction_train, 
+         "w" : w, 
+         "b" : b,
+         "learning_rate" : learning_rate,
+         "num_iterations": num_iterations}
+    
+    return d
+
+
+def run_etl():
+    # Loading the data (cat/non-cat)
+    train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+
+    m_train = train_set_y.shape[1]
+    m_test = test_set_y.shape[1]
+    num_px = train_set_x_orig.shape[1]
+
+    train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+    test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+
+    train_set_x = train_set_x_flatten / 255.
+    test_set_x = test_set_x_flatten / 255.
+
+    return train_set_x, test_set_x, train_set_y, test_set_y
+
+def testMe():    
+    w, b, X, Y = np.array([[1.],[2.]]), 2., np.array([[1.,2.,-1.],[3.,4.,-3.2]]), np.array([[1,0,1]])
+    grads, cost = propagate(w, b, X, Y)
+    print ("dw = " + str(grads["dw"]))
+    print ("db = " + str(grads["db"]))
+    print ("cost = " + str(cost))
+
+    params, grads, costs = optimize(w, b, X, Y, num_iterations= 100, learning_rate = 0.009, print_cost = False)
+    
+    print ("w = " + str(params["w"]))
+    print ("b = " + str(params["b"]))
+    print ("dw = " + str(grads["dw"]))
+    print ("db = " + str(grads["db"]))
+
+# testMe()
+
+def run_model():
+    train_set_x, test_set_x, train_set_y, test_set_y = run_etl()
+    num_iterations = 2000
+    learning_rate = 0.005
+    print_cost = True
+    d = model(train_set_x,train_set_y, test_set_x, test_set_y,num_iterations, learning_rate,print_cost)
+    
+    print(d)
+
+
+run_model()
---
+++ code/Neural Networks and Deep Learning/week2/code/lr_utils.py
@@ -0,0 +1,20 @@
+import numpy as np
+import h5py
+from os import path , getcwd
+    
+    
+def load_dataset():
+    train_file_path = path.join(path.dirname(__file__),'..','data/train_catvnoncat.h5')
+    train_dataset = h5py.File(train_file_path, "r")
+    # return train_dataset
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+    test_file_path = path.join(path.dirname(__file__),'..','data/test_catvnoncat.h5')
+    
+    test_dataset = h5py.File(test_file_path, "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+    classes = np.array(test_dataset["list_classes"][:]) # the list of classes    
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))    
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
